df2eb10681a994283c3ba079199a886c04521041
tutrungtranvn <tutrung.leo@gmail.com>
2025-09-04 15:27:08 +0700

    update dags with newest version

--- dags/hello.py
+++ dags/hello.py
@@ -5,7 +5,7 @@ from datetime import datetime
 with DAG(
     dag_id="my_first_simple_dag",
     start_date=datetime(2023, 1, 1),
-    schedule_interval=None,  # Run manually or on a specific schedule
+    schedule=None,  # Run manually or on a specific schedule
     catchup=False,
     tags=["example"],
 ) as dag:
