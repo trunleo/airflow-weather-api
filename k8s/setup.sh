590f87b1a14c0516803280cb9e10068fd7dcc0e2
tutrungtranvn <tutrung.leo@gmail.com>
2025-09-04 16:37:16 +0700

    update dags with newest version

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -8,3 +8,18 @@ helm install airflow apache-airflow/airflow \
     --create-namespace \
     --debug \
     --timeout 10m01s
+
+# Set up gitsync for airflow
+echo -n "github-username" | base64
+echo -n "github-token" | base64
+
+# Create secret
+kubectl apply -f k8s/secret.yaml --namespace airflow
+
+# Show values file
+helm show values airflow -n airflow > k8s/values.yaml
+
+helm upgrade --install airflow apache-airflow/airflow \
+    -n airflow -f k8s/values.yaml \
+    --debug \
+    --timeout 10m02s
