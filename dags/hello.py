a24559ffe69e442ecab86771dba35020a4d45775
tutrungtranvn <tutrung.leo@gmail.com>
2025-09-04 15:12:49 +0700

    add simple dags

---
+++ dags/hello.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="my_first_simple_dag",
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,  # Run manually or on a specific schedule
+    catchup=False,
+    tags=["example"],
+) as dag:
+    # Define tasks
+    start_task = BashOperator(
+        task_id="start_message",
+        bash_command="echo 'Starting the DAG!'",
+    )
+
+    process_data = BashOperator(
+        task_id="process_data",
+        bash_command="echo 'Processing some data...'",
+    )
+
+    end_task = BashOperator(
+        task_id="end_message",
+        bash_command="echo 'DAG finished successfully!'",
+    )
+
+    # Define task dependencies
+    start_task >> process_data >> end_task
