2908de6b874c12af345f18e04efa0733f363207c
trung-tran-vns <trung.tran@vnsilicon.net>
2025-10-16 00:30:41 +0700

    change: remove retry

--- dags/wt_test_notification.py
+++ dags/wt_test_notification.py
@@ -22,7 +22,7 @@ default_args = {
     "depends_on_past": False,
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 1,
+    "retries": 0,
     "retry_delay": timedelta(minutes=5),
     "start_date": datetime(2025, 10, 12),
     "catchup": False,
