ec27b55c8dfc2d10073dd5a1a5ece92d9968827c
trung-tran-vns <trung.tran@vnsilicon.net>
2025-10-16 14:33:08 +0700

    add: libary

--- include/weather/core/etl/references_tbl.py
+++ include/weather/core/etl/references_tbl.py
@@ -3,18 +3,6 @@ import logging
 from typing import Any
 
 logger = logging.getLogger(__name__)
-
-def create_connection(postgres_conn_id: str = "WEATHER_POSTGRES_CONN") -> WeatherDBManager:
-    """Create and return a WeatherDBManager instance."""
-    try:
-        db_manager = WeatherDBManager(
-            postgres_conn_id=postgres_conn_id
-        )
-        logger.info("Database connection created successfully.")
-        return db_manager
-    except Exception as e:
-        logger.error(f"Error creating database connection: {e}")
-        raise
     
 def get_reference_table(db_manager: WeatherDBManager, ref_tbl_name: str = "", **kwargs) -> Any:
     """Fetch and log all records from the specified reference table."""
