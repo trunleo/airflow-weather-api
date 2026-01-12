bb471cb205e68a05363a7c6073d933e88f4d46bc
trunleo <tutrung.leo@gmail.com>
2026-01-12 10:31:10 +0700

    .

--- dags/marketprice/transform_data.py
+++ dags/marketprice/transform_data.py
@@ -40,11 +40,15 @@ def upsert_table(df, table_name: str = "", conflict_key: list = ["id"], **contex
     return count
 
 def transform_product_tbl(**context):
-    daily_product_prices_df = get_table("daily_product_prices", schema="public")
+    mapping_df = get_table("mapping_list", schema="public")
+    logger.info("First 10 rows of mapping table: %s", mapping_df.head(10))
+
     
+    daily_product_prices_df = get_table("daily_product_prices", schema="public")
     re_col_list = ['date_time', 'category_name', 'price_type', 'product_id', 'product_name']
     product_df = daily_product_prices_df[re_col_list].drop_duplicates()
     logger.info("First 10 rows of product table: %s", product_df.head(10))
+    
     return product_df
     
 # def transform_product_prices(**context):    
